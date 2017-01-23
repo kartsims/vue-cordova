2626de60e640a2b45dfc1208ddcea02e7e8bf236
Mike Borthwick <mike@strixy.com>
2017-01-23 14:18:27 -0700

    Adds dbmeter plugin

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const pluginsList = [
   'cordova-plugin-camera',
   'cordova-plugin-device',
   'cordova-plugin-geolocation',
-  'cordova-plugin-contacts'
+  'cordova-plugin-contacts',
+  'cordova-plugin-dbmeter',
 ]
 
 exports.install = (Vue, options) => {
