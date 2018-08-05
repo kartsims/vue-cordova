aec3b4702374df70f027377fcde4cad17e6c7d0a
Juan David Saab <juan.saab@aguayo.co>
2018-08-05 15:06:24 -0500

    [Webpack] - Fixes problem with babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['es2015']
         }
